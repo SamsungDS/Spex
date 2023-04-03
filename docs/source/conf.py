6f2534ec8a8fed79d0db9889b180f4be82c327d3
Jesper Wendel Devantier <j.devantier@samsung.com>
2023-04-03 09:03:26 +0200

    sphinx docs and updated dev reqs

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Spex'
+copyright = '2023, NVMe Working Group'
+author = 'NVMe Working Group'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
+
+html_css_files = ["css/custom.css"]
+html_js_files = ["scripts/custom.js"]
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. Spex documentation master file, created by
+   sphinx-quickstart on Fri Mar 24 09:26:40 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Spex's documentation!
+================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
