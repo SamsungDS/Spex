8fe01aa953ac255e0a7f96ba669f48dde01014cf
Jesper Wendel Devantier <j.devantier@samsung.com>
2023-06-29 19:08:47 +0200

    scripts - check if running in nix env

--- scripts/format.sh
+++ scripts/format.sh
@@ -2,7 +2,12 @@
 on_err() {
     set +x
     if [ ! -n "$SPEX_NIX_ENV" ]; then
-        echo "WARN> script not running in Nix development environment."
+        echo "WARN> script encountered an error during execution."
+        echo ""
+        echo "This script exited due to an error. However, this script was"
+        echo "also run outside of the nix development environment."
+        echo ""
+        echo "In case of an abnormal/unexpected error - please check your environment."
     fi
 }
 trap 'on_err' ERR
