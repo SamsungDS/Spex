155a49c3ecb888b35e81c77193cadaf9daf329bf
Simon A. F. Lund <os@safl.dk>
2023-07-10 12:49:10 +0200

    refactor: move spex into 'src' folder
    Signed-off-by: Simon A. F. Lund <os@safl.dk>

--- scripts/check.sh
+++ scripts/check.sh
@@ -5,8 +5,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 set -ex
 
-flake8 spex --extend-ignore=E203,F401,F811,E501
-mypy spex
+flake8 src/spex --extend-ignore=E203,F401,F811,E501
+mypy src/spex
 
-black spex --check
-isort spex --profile=black --check
+black src/spex --check
+isort src/spex --profile=black --check
