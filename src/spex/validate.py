b3e8c4af7911d09b9187613a7eda8d3c6211b8bc
Morten B. Rasmussen <m.rasmussen@samsung.com>
2024-03-20 15:45:45 +0100

    Add jsonschema validation for generated json
    Move the stage2.schema.json from the root of the project into the src
    directory, include it explicitly in MANIFEST.in to have it included
    in the resulting nvme-spex package.
    
    Add jsonschema as dependency.
    
    Add cmdline flag `--validate-json`. When running spex with the new flag
    it will validate that the resulting json aligns with the schema.

---
+++ src/spex/validate.py
@@ -0,0 +1,12 @@
+from importlib import resources
+from pathlib import Path
+from jsonschema import validate
+import json
+
+
+def validate_json(document_path: Path):
+    schema = (
+        resources.files("spex.resources").joinpath("stage2.schema.json").read_text()
+    )
+    document = document_path.read_text()
+    validate(json.loads(document), json.loads(schema))
