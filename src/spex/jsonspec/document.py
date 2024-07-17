22ae02ed22f4a09aab8aea01b5e5ee440fadd992
Jesper Wendel Devantier <j.devantier@samsung.com>
2024-07-17 10:25:47 +0200

    clean column headers of extra whitespace and linebreaks
    I've run the diffing tool on outputs generated before and after this
    change, neither amount nor actual linting errors change.
    This does, however, make it easier to match in the future.
    
    Signed-off-by: Jesper Wendel Devantier <j.devantier@samsung.com>

--- src/spex/jsonspec/document.py
+++ src/spex/jsonspec/document.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-from re import compile as re_compile
+from re import compile as re_compile, sub
 from typing import TYPE_CHECKING, Dict, Iterator, List, Optional, Tuple, Type, TypeAlias
 
 from loguru import logger
@@ -221,6 +221,8 @@ class DocumentParser:
         """
 
         def normalize_hdr(hdr: str) -> str:
+            # some headers have newlines and use spaces for indentation, strip all of that
+            hdr = sub(' +', ' ', hdr.replace('\n', ' ').replace('\r', '')).strip()
             replacement = self.tbl_normalize_mappings.get(hdr, None)
             if replacement is not None:
                 self.linter.add_issue(
